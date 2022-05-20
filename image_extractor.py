77862f25d9c5eeba0bbabf5b0fb5795dfce76895
EdoardoMarchetti <62024316+EdoardoMarchetti@users.noreply.github.com>
2022-05-20 18:49:04 +0200

    Update image_extractor.py

--- image_extractor.py
+++ image_extractor.py
@@ -3,9 +3,16 @@ from PIL import Image
 
 print(os.getcwd())
 
-names = os.listdir("data/GTA5_modified/images")
+names = os.listdir("data/GTA5_modified/labels")
+names.sort()
+
+f = open("data/GTA5_modified/train.txt", "w+")
+
 
 for name in names:
-    label_from_GTA5 = Image.open(f"data/GTA5/labels/"+name)
-    label_from_GTA5.save(f"data/GTA5_modified/labels/{name}")
+    #label_from_GTA5 = Image.open(f"data/GTA5/labels/"+name)
+    #label_from_GTA5.save(f"data/GTA5_modified/labels/{name}")
+    f.write(name+"\n")
+
+f.close()
 
